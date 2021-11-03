3351eaa67885e0d1a70afc2f24292f85be6c1371
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2021-11-03 16:42:20 +0100

    more agents
    create the java files for the 4 agents that will be used

---
+++ ProjectCode/DataManagerAgent.java
@@ -0,0 +1,12 @@
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.util.Logger;
+
+public class DataManager extends Agent {
+
+}
