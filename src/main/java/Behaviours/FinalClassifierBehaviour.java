b82e88c716c69cd2b8624e6dc093ee5ed019a7fe
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2021-12-31 11:01:27 +0100

    final classiffier behaviour

--- src/main/java/Behaviours/FinalClassifierBehaviour.java
+++ src/main/java/Behaviours/FinalClassifierBehaviour.java
@@ -1,19 +1,29 @@
 package Behaviours;
 
-import Agents.ClassifierAgent;
+import Agents.FinalClassifierAgent;
+import Utils.AuditDataRowWithPrediction;
+import Utils.Configuration;
+import Utils.DatasetManager;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
 import weka.classifiers.Classifier;
-import weka.classifiers.trees.J48;
 import weka.core.Instances;
-import weka.core.converters.CSVLoader;
+
+import java.util.logging.Level;
+
+import static Utils.PredictionEvaluatorUtils.evaluateAndPrintPredictionResults;
+
 
 public class FinalClassifierBehaviour {
-    private ClassifierAgent classifierAgent;
+    private FinalClassifierAgent finalclassifierAgent;
     private boolean send = true;
-    
-    public FinalClassifierBehaviour(ClassifierAgent classifierAgent) {
-        this.classifierAgent = classifierAgent;
+    private int numClassifiers;
+    private int numResultsReceived = 0;
+
+    public FinalClassifierBehaviour(FinalClassifierAgent classifierAgent) {
+        this.finalclassifierAgent = classifierAgent;
     }
 
     @Override
@@ -21,7 +31,7 @@ public class FinalClassifierBehaviour {
         if (this.send) {
             ACLMessage message = new ACLMessage(ACLMessage.INFORM);
             message.addReceiver(new AID("dataManagerAgent", AID.ISLOCALNAME));
-            this.classifierAgent.send(message);
+            this.finalclassifierAgent.send(message);
             this.send = false;
             //TODO someone
         }
@@ -29,4 +39,50 @@ public class FinalClassifierBehaviour {
     private void trainModel() {
 
     }
+
+    private void testModel(){
+
+    }
+
+    private void waitForResultsFromClassifierAgents() {
+        while(numResultsReceived < numClassifiers) {
+            MessageTemplate performativeFilter = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+            ACLMessage message = this.finalclassifierAgent.receive(performativeFilter);
+            if (message != null && message.getSender().getLocalName().equals("classifierAgent")) {
+                try {
+                    Configuration configuration = (Configuration) message.getContentObject();
+                    this.numClassifiers = configuration.getNumClassifiers();
+                    this.datasetManager = new DatasetManager(configuration);
+
+                    this.dataManagerAgentState = DataManagerAgentState.WaitingForTrain;
+                } catch (UnreadableException e) {
+                    e.printStackTrace();
+                }
+                numResultsReceived++;
+            } else {
+                this.block();
+            }
+        }
+    }
+
+    private void waitForPetitionResults() {
+        MessageTemplate messageTemplate = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
+        ACLMessage message = this.userAgent.receive(messageTemplate);
+        if (message != null && message.getSender().getLocalName().equals("dataManagerAgent")) {
+            //TODO: get predictions for the 15 data rows
+            try {
+                AuditDataRowWithPrediction[] petitionResults = (AuditDataRowWithPrediction[]) message.getContentObject();
+                logger.log(Level.INFO, "Received classifications for the chosen ");
+                evaluateAndPrintPredictionResults(petitionResults);
+            }
+            catch (UnreadableException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            this.block();
+        }
+    }
+
+
 }
